c42b58d293753798c8240cedbe2a54370e8529c0
karam084 <karam.ezz84@gmail.com>
2022-03-14 23:01:28 +0200

    Add display and store books

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-import data from './data.js'
+import data from './data.js';
 
-const addBook = () => {
-  //add book
-}
+const form = document.querySelector('#book-form');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookSection = document.querySelector('.books');
+
+const addBook = (author, title) => {
+  data.push({ author, title });
+  return data.findIndex((book) => book.author === author);
+};
 
 const deleteBook = () => {
   //delete book
-}
+};
 
-const displayBook = () => {
-  //display book
-}
+const displayBook = (author, title, indx) => {
+  const bookCard = `
+    <div class="book-card">
+      <h2 class="book">${title}</h2>
+      <h3 class="author">${author}</h3>
+      <button class="btn-remove" data-index="${indx}" type="button">Remove</button>
+      <hr />
+    </div>
+  `;
+  bookSection.insertAdjacentHTML('beforeend', bookCard);
+};
+
+const preserveData = () => {
+  //store data on localstorage
+};
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  const bookIndex = addBook(title, author);
+  displayBook(title, author, bookIndex);
+});

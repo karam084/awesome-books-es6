0e04aed49a15c716609703a761d775387300b1ff
Henry Nawrocki <hdev@sztanski.com>
2022-03-14 16:12:28 -0500

    Save books objects array to local storage

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-import data from './data.js';
+import data from './data.js'
 
-const form = document.querySelector('#book-form');
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const bookSection = document.querySelector('.books');
+const form = document.querySelector('#book-form')
+const bookTitle = document.querySelector('#title')
+const bookAuthor = document.querySelector('#author')
+const bookSection = document.querySelector('.books')
 
 const addBook = (author, title) => {
-  data.push({ author, title });
-  return data.findIndex((book) => book.author === author);
-};
+  data.push({ author, title })
+  return data.findIndex((book) => book.author === author)
+}
 
 const deleteBook = () => {
   //delete book
-};
+}
 
 const displayBook = (author, title, indx) => {
   const bookCard = `
@@ -22,17 +22,19 @@ const displayBook = (author, title, indx) => {
       <button class="btn-remove" data-index="${indx}" type="button">Remove</button>
       <hr />
     </div>
-  `;
-  bookSection.insertAdjacentHTML('beforeend', bookCard);
-};
+  `
+  bookSection.insertAdjacentHTML('beforeend', bookCard)
+}
 
-const preserveData = () => {
-  //store data on localstorage
-};
+const preserveBookData = () => {
+  const booksData = JSON.stringify(data)
+  window.localStorage.setItem('books', booksData)
+}
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = bookTitle.value;
-  const author = bookAuthor.value;
-  const bookIndex = addBook(title, author);
-  displayBook(title, author, bookIndex);
-});
+  e.preventDefault()
+  const title = bookTitle.value
+  const author = bookAuthor.value
+  const bookIndex = addBook(title, author)
+  preserveBookData()
+  displayBook(title, author, bookIndex)
+})

d3df134685ec5e13186342b55ceea598b36ce96d
Henry Nawrocki <hdev@sztanski.com>
2022-03-14 16:31:01 -0500

    Start remove book functionality

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const form = document.querySelector('#book-form')
 const bookTitle = document.querySelector('#title')
 const bookAuthor = document.querySelector('#author')
 const bookSection = document.querySelector('.books')
+const removeBookBtns = document.querySelectorAll('.btn-remove')
 
 const addBook = (author, title) => {
   data.push({ author, title })
   return data.findIndex((book) => book.author === author)
-}
-
-const deleteBook = () => {
-  //delete book
 }
 
 const displayBook = (author, title, indx) => {
@@ -19,22 +16,33 @@ const displayBook = (author, title, indx) => {
     <div class="book-card">
       <h2 class="book">${title}</h2>
       <h3 class="author">${author}</h3>
-      <button class="btn-remove" data-index="${indx}" type="button">Remove</button>
+      <button class="btn-remove" id="btn-rem-${indx}" data-index="${indx}" type="button">Remove</button>
       <hr />
     </div>
   `
   bookSection.insertAdjacentHTML('beforeend', bookCard)
+  return `btn-rem-${indx}`
 }
 
 const preserveBookData = () => {
   const booksData = JSON.stringify(data)
   window.localStorage.setItem('books', booksData)
 }
+
+// Remove book function helper
+const removeBook = (event) => {
+  console.log('in removeBook')
+  console.log(event.target)
+}
+
+// Event lister to add books and save them
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const title = bookTitle.value
   const author = bookAuthor.value
   const bookIndex = addBook(title, author)
   preserveBookData()
-  displayBook(title, author, bookIndex)
+  const btnRemId = displayBook(title, author, bookIndex)
+  // Event listener to remove a book
+  document.getElementById(btnRemId).addEventListener('click', removeBook)
 })
